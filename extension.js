6eee0660fcab877182a5b18be71ad22531c5c20f
v_zqianchen(陈志强) <v_zqianchen@tencent.com>
2021-07-26 18:17:57 +0800

    U 解析snippet获取自动补全的项

--- extension.js
+++ extension.js
@@ -1,110 +1,125 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 const vscode = require('vscode');
+const path = require('path');
+const fs = require('fs');
+let matchAttr = /"([a-zA-Z]+)": (.+),/;
+const matchComment = /\/\/\s(.+)/;
+const matchComponentExit = /component\s*:(.+),/;
+const matchAttrExit = /attributes\s*:/;
+const matchValidityExit = /validity\s8:/;
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
+// node_modules/@tencent/weadmin-components-bizadmin/src/v2/components/button/button.vue
+// 'origincalendar',
+// 'selectlist',
+// 'upload-item',
+let MAIN_COMPONENT_NAMES = [
+  'aggregatedata',
+  'button',
+  'calendar',
+  'chart',
+  'checkbox',
+  'container',
+  'input',
+  'link',
+  'vpagination',
+  'processor',
+  'progress',
+  'querybuilder',
+  'radio',
+  // 'resourceMark',
+  'richtext',
+  'schedule',
+  'select',
+  'tab',
+  'table',
+  'tag',
+  'text',
+  'textarea',
+  'tree',
+  'upload',
+];
 
-/**
- * @param {vscode.ExtensionContext} context
- */
 function activate(context) {
   console.log('cls-helper running');
 
-  // # 写死匹配文本
-  const provider1 = vscode.languages.registerCompletionItemProvider('plaintext', {
-    provideCompletionItems(document, position, token, context) {
-      // a simple completion item which inserts `Hello World!`
-      const simpleCompletion = new vscode.CompletionItem('Hello World!');
-
-      // a completion item that inserts its text as snippet,
-      // the `insertText`-property is a `SnippetString` which will be
-      // honored by the editor.
-      const snippetCompletion = new vscode.CompletionItem('Good part of the day');
-      snippetCompletion.insertText = new vscode.SnippetString(
-        'Good ${1|morning,afternoon,evening|}. It is ${1}, right?'
-      );
-      snippetCompletion.documentation = new vscode.MarkdownString(
-        'Inserts a snippet that lets you select the _appropriate_ part of the day for your greeting.'
-      );
-
-      // a completion item that can be accepted by a commit character,
-      // the `commitCharacters`-property is set which means that the completion will
-      // be inserted and then the character will be typed.
-      const commitCharacterCompletion = new vscode.CompletionItem('console');
-      commitCharacterCompletion.commitCharacters = ['.'];
-      commitCharacterCompletion.documentation = new vscode.MarkdownString('Press `.` to get `console.`');
-
-      // a completion item that retriggers IntelliSense when being accepted,
-      // the `command`-property is set which the editor will execute after
-      // completion has been inserted. Also, the `insertText` is set so that
-      // a space is inserted after `new`
-      const commandCompletion = new vscode.CompletionItem('new');
-      commandCompletion.kind = vscode.CompletionItemKind.Keyword;
-      commandCompletion.insertText = 'new ';
-      commandCompletion.command = { command: 'editor.action.triggerSuggest', title: 'Re-trigger completions...' };
-
-      // return all completion items as array
-      return [simpleCompletion, snippetCompletion, commitCharacterCompletion, commandCompletion];
-    },
+  let snippetPath = path.resolve(`${process.cwd()}/.vscode/snippets.code-snippets`);
+  fs.access(snippetPath, constants.F_OK | constants.W_OK, err => {
+    if (err) {
+      console.error(`${snippetPath} ${err.code === 'ENOENT' ? 'does not exist' : 'is read-only'}`);
+    } else {
+      let parseResult = JSON.parse(fs.readFileSync(snippetPath));
+      let complements = {};
+      MAIN_COMPONENT_NAMES.forEach(componentName => {
+        getComplementItem(componentName, 'attributes', parseResult, complements);
+        getComplementItem(componentName, 'validity', parseResult, complements);
+      });
+    }
   });
 
-  /**
-   * 
-   *    return {
-      label: tag,
-      sortText: `0${id}${tag}`,
-      insertText: new SnippetString(prettyHTML('<' + snippets.join(''), {indent_size: this.size}).substr(1)),
-      kind: CompletionItemKind.Snippet,
-      detail: `element-ui ${tagVal.version ? `(version: ${tagVal.version})` : ''}`,
-      documentation: tagVal.description
-    };
-   */
-
-  const provider2 = vscode.languages.registerCompletionItemProvider(
-    'plaintext',
+  // # 根据指定模式，动态生成匹配文本
+  const provider = vscode.languages.registerCompletionItemProvider(
+    'vue',
     {
       provideCompletionItems(document, position) {
-        // get all text until the `position` and check if it reads `console.`
-        // and if so then complete if `log`, `warn`, and `error`
-        const linePrefix = document.lineAt(position).text.substr(0, position.character);
-        if (!linePrefix.endsWith('console.')) {
+        const curLine = document.lineAt(position).text.substr(0, position.character);
+        // 当前行有 'component' +'attributes' / 'validity'
+        if ((curLine.includes('component') && curLine.includes('attributes')) || curLine.includes('validity')) {
+          let componentName = curLine.match(matchComponentExit);
+
           return undefined;
         }
 
-        console.log('linePrefix', linePrefix);
+        /**
+         * 1. 仅指定 attributes
+         * 1.1 当前行 或 向上找, 可以找到 attribute
+         * 1.2 发现 '}'
+         *
+         */
 
-        // 这个调用方法返回对象
-        console.log(new vscode.CompletionItem('log', vscode.CompletionItemKind.Method));
-
-        return [
-          new vscode.CompletionItem('log', vscode.CompletionItemKind.Method),
-          new vscode.CompletionItem('warn', vscode.CompletionItemKind.Method),
-          new vscode.CompletionItem('error', vscode.CompletionItemKind.Method),
-        ];
+        // new vscode.CompletionItem => 返回对象
+        // 匹配结果
+        // icon类型: Value / Method / Snippet
+        return {
+          label: tag, // 提示符
+          sortText: `0${tag}`, // 检索见过排序
+          insertText: new SnippetString(prettyHTML('<' + snippets.join(''), { indent_size: this.size }).substr(1)), // 插入文本
+          kind: CompletionItemKind.Snippet, // 文本图标
+          // tagVal.version
+          detail: `cls-ui`,
+          documentation: tagVal.description, // 描述
+        };
       },
     },
-    '.' // triggered whenever a '.' is being typed
+    ' ',
+    ':'
   );
+  /**
+   * 1. 通过 未写完的属性名联想（attributes.hi） 。拉取 snippet；
+   * 2. 通过 写完的属性名联想（attributes.hide）。拉取 值的范围；
+   */
+  /**
+   *
+   * ' ',
+   * ':',
+   *
+   * '"',
+   *"'"
+   */
 
-  context.subscriptions.push(provider1, provider2);
+  context.subscriptions.push(provider);
 }
-
-// this method is called when your extension is deactivated
 function deactivate() {}
 
-function getPreTag() {
-  let line = this._position.line;
+function getPreTag(position) {
+  // position => {line, character}
+  let line = position.line;
   let tag = null;
-  let txt = this.getTextBeforePosition(this._position);
-
-  console.log('this._position.line', this._position.line, 'line', line);
+  let txt = this.getTextBeforePosition(position);
 
   /**
    * line 往上最多回溯10行，直到找到匹配的 标签
    */
-  while (this._position.line - line < 10 && line >= 0) {
-    if (line !== this._position.line) {
+  while (position.line - line < 10 && line >= 0) {
+    if (line !== position.line) {
       txt = this._document.lineAt(line).text;
     }
 
@@ -116,6 +131,60 @@ function getPreTag() {
     line--;
   }
   return;
+}
+
+function getComplementItem(componentName = '', attrName = '', parseResult = {}, collection = {}) {
+  // { button: { attributes: { hide: complementItem }, validity}, ... }
+  let keyName = `@cls cls-${componentName}-${attrName}`;
+  if (parseResult[keyName]) {
+    let { body } = parseResult[keyName];
+
+    let complementItem = handleSnippetBody(componentName, attrName, body);
+    if (collection[componentName][attrName]) {
+      Object.assign(collection[componentName][attrName], complementItem);
+    } else {
+      collection[componentName][attrName] = complementItem;
+    }
+  }
+}
+function handleSnippetBody(componentName = '', attrType = '', body = []) {
+  body.forEach((curStr, index) => {
+    let matchAttrResult = curStr.match(matchAttr);
+    let attrName = matchAttrResult ? matchAttrResult[1] : '';
+    let attrValue = matchAttrResult ? matchAttrResult[2] : '';
+
+    let matchCommentResult = curStr.match(matchComment);
+    let comment = matchCommentResult ? matchCommentResult[1] : '';
+
+    // console.log(attrName, attrValue, comment);
+
+    if (attrName && attrValue) {
+      return {
+        attrName: {
+          snippet: {
+            detail: `cls-ui`,
+            kind: CompletionItemKind.Snippet,
+            label: componentName,
+            sortText: `0${componentName}${attrType}${attrName}`,
+            insertText: new SnippetString(`${attrName}: \${2:${attrValue}}`),
+            documentation: comment || '',
+          },
+          value: {
+            detail: `cls-ui`,
+            kind: CompletionItemKind.Snippet,
+            label: componentName,
+            sortText: `0${componentName}${attrType}${attrName}`,
+            // new SnippetString(`\${1:${attrValue}}`)
+            insertText: attrValue,
+            documentation: comment || '',
+          },
+        },
+      };
+    }
+  });
+
+  var result = JSON.parse(body.join(''));
+  console.log('result', result, typeof result);
 }
 
 /**
